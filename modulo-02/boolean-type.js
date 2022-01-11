9740315bcfc05c767a00ce8cde1b1ba926ec87b2
ThomasDantas <thomas@paipe.co>
2022-01-11 11:26:24 -0300

    modulo 2

---
+++ modulo-02/boolean-type.js
@@ -0,0 +1,13 @@
+// ==> Exemplo 01
+var tarefaConcluida = true;
+var tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+// ==> Exemplo 02
+var concluido = false;
+if (!concluido) {
+    console.log('Tarefa foi concluída com sucesso!');
+}
+else {
+    console.log('Tarefa Pendente!');
+}
